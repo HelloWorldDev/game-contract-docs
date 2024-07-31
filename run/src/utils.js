f5405e13781af33269307f82d754f9610438c161
troubleMakerrr <972824757@qq.com>
2024-07-31 15:15:16 +0800

    push buy

--- run/src/utils.js
+++ run/src/utils.js
@@ -71,3 +71,33 @@ export async function sendAndConfirmOptimalTransaction({ connection, ixs, payer,
 
     return txsig;
 }
+
+//The amount of sols entered during purchase corresponds to how many current coins can be purchased (including handling fees)
+//inputValue is the number of coins entered in the input
+export function calSol2Token(token_supply, sol_balance, inputValue) {
+    let current_supply = token_supply;
+    let current_sol_balance = sol_balance;
+    let buy_amount_lamports = numToString(new BigNumber(inputValue * 0.99).times(1e9).dp(0, BigNumber.ROUND_DOWN));
+    let m = 30000000000;
+    let p = Number(current_sol_balance) + Number(buy_amount_lamports);
+    let n = 1073000000000000;
+    let np = n * p;
+    let m_plus_p = Number(m) + Number(p);
+    let tokens_bought = numToString(new BigNumber(Number(np / m_plus_p) - Number(current_supply)).div(1e6));
+    return tokens_bought;
+}
+
+//The current amount of coins entered when selling corresponds to how many sols can be sold (including handling fees)
+//inputValue is the number of coins entered in the input
+export function calToken2Sol(token_supply, sol_balance, inputValue) {
+    let current_supply = token_supply;
+    let current_sol_balance = sol_balance;
+    let buy_amount_lamports = numToString(new BigNumber(inputValue).times(1e6).dp(0, BigNumber.ROUND_DOWN));
+    let k = Number(current_supply) - Number(buy_amount_lamports);
+    let m = 30000000000;
+    let n = 1073000000000000;
+    let km = k * m;
+    let n_minus_k = Number(n) - Number(k);
+    let lamports_received = numToString(new BigNumber(Number(current_sol_balance) - Number(km / n_minus_k)).div(1e9));
+    return lamports_received * 0.99;
+}
